dd398979b58ae8e268094dc0b9cac2738b47daff
lzichong <102765531+lzichong@users.noreply.github.com>
2022-04-08 13:26:01 -0700

    [VCDA-3513] Handle trailing slashes to avoid broken API paths (#63)
    * [VCDA-3513] Handle trailing slashes to avoid broken API paths
    
    * Updated changes to directly trim config.vcd.host instead of creating new function
    
    Signed-off-by: lzichong <lzichong@vmware.com>

--- pkg/config/cloudconfig.go
+++ pkg/config/cloudconfig.go
@@ -16,10 +16,10 @@ import (
 
 // VCDConfig :
 type VCDConfig struct {
-	Host string `yaml:"host"`
-	VDC  string `yaml:"vdc"`
-	Org  string `yaml:"org"`
-	UserOrg      string // this defaults to Org or a prefix of User
+	Host    string `yaml:"host"`
+	VDC     string `yaml:"vdc"`
+	Org     string `yaml:"org"`
+	UserOrg string // this defaults to Org or a prefix of User
 
 	// It is allowed to pass the following variables using the config. However,
 	// that is unsafe security practice. However there can be user scenarios and
@@ -35,7 +35,7 @@ type VCDConfig struct {
 
 	VDCNetwork string `yaml:"network"`
 	VIPSubnet  string `yaml:"vipSubnet"`
-	VAppName  string  `yaml:"vAppName"`
+	VAppName   string `yaml:"vAppName"`
 }
 
 // Ports :
@@ -98,7 +98,7 @@ func ParseCloudConfig(configReader io.Reader) (*CloudConfig, error) {
 	if err = decoder.Decode(&config); err != nil {
 		return nil, fmt.Errorf("unable to decode yaml file: [%v]", err)
 	}
-
+	config.VCD.Host = strings.TrimRight(config.VCD.Host, "/")
 	return config, nil
 }
 
