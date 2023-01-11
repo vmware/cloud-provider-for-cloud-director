6ac052e0b1747f70c643513e64bd025ac3be0ba5
lzichong <102765531+lzichong@users.noreply.github.com>
2023-01-11 10:13:47 -0800

    [VKCI-89] Skeleton structure and starter code for automation common code (#182)
    * Initial skeleton for automation common code, includes basic RDE extraction, VCD client, go-client initialization
    
    Signed-off-by: lzichong <lzichong@vmware.com>
    
    * Minor change to clientset
    
    Signed-off-by: lzichong <lzichong@vmware.com>
    
    * Rename test_client file to client
    
    Signed-off-by: lzichong <lzichong@vmware.com>
    
    * CR of Yan: Update DeployParams to include VolumeParams and ContainerParams
    
    Signed-off-by: lzichong <lzichong@vmware.com>
    
    * CR of Yan: Rename vcdResourceSetArrMapping to vcdResourceSetMap
    
    Signed-off-by: lzichong <lzichong@vmware.com>
    
    * Update context.TODO() as params
    
    Signed-off-by: lzichong <lzichong@vmware.com>
    
    * Refactor kube client creation
    
    Signed-off-by: lzichong <lzichong@vmware.com>
    
    * Add method to get cluster name as go-client has no way to retrieve it
    
    Signed-off-by: lzichong <lzichong@vmware.com>
    
    * Address Richard's CR: Expose method in rde_utils instead of client.go
    
    Signed-off-by: lzichong <lzichong@vmware.com>
    
    * Move GetVCDResourceSet() into rde_utils
    
    Signed-off-by: lzichong <lzichong@vmware.com>
    
    * Expose main method for GetComponentMapInStatus
    
    Signed-off-by: lzichong <lzichong@vmware.com>
    
    * Refactored left out updates
    
    Signed-off-by: lzichong <lzichong@vmware.com>
    
    * Address Arun CR: Update error msg, re-use getRdeById(), expose GetKubeConfig(), add comment regarding RDE versions
    
    Signed-off-by: lzichong <lzichong@vmware.com>
    
    * Fix typo
    
    Signed-off-by: lzichong <lzichong@vmware.com>
    
    Signed-off-by: lzichong <lzichong@vmware.com>

---
+++ pkg/testingsdk/client.go
@@ -0,0 +1,82 @@
+package testingsdk
+
+import (
+	"context"
+	"fmt"
+	"github.com/vmware/cloud-provider-for-cloud-director/pkg/vcdsdk"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type TestClient struct {
+	VcdClient *vcdsdk.Client
+	Cs        kubernetes.Interface
+	ClusterId string
+}
+
+type VCDAuthParams struct {
+	Host         string
+	OvdcName     string
+	OrgName      string
+	Username     string
+	RefreshToken string
+	UserOrg      string
+	GetVdcClient bool // This will need to be set to true as it's needed for CSI, but may not be needed for other use cases
+}
+
+type DeployParams struct {
+	Name            string
+	labels          map[string]string
+	volumeParams    VolumeParams
+	containerParams ContainerParams
+}
+type VolumeParams struct {
+	volumeName string
+	pvcRef     string
+	mountPath  string
+}
+
+type ContainerParams struct {
+	ContainerName  string
+	ContainerImage string
+	ContainerPort  int32
+	Args           []string
+}
+
+func NewTestClient(params *VCDAuthParams, clusterId string) (*TestClient, error) {
+	client, err := getTestVCDClient(params)
+	if err != nil {
+		return nil, fmt.Errorf("error occured while generating client using [%s:%s] for cluster [%s]: [%v]", params.Username, params.UserOrg, clusterId, err)
+	}
+
+	kubeConfig, err := GetKubeconfigFromRDEId(context.TODO(), client, clusterId)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get kubeconfig from RDE [%s]: [%v]", clusterId, err)
+	}
+
+	cs, err := createKubeClient(kubeConfig)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create clientset using RESTConfig generated from kubeconfig for cluster [%s]: [%v]", clusterId, err)
+	}
+	return &TestClient{
+		VcdClient: client,
+		Cs:        cs,
+		ClusterId: clusterId,
+	}, nil
+}
+
+func (tc *TestClient) GetClusterName(ctx context.Context, clusterId string) (string, error) {
+	rde, err := getRdeById(ctx, tc.VcdClient, clusterId)
+	if err != nil {
+		return "", fmt.Errorf("unable to get defined entity by clusterId [%s]: [%v]", clusterId, err)
+	}
+	return rde.Name, nil
+}
+
+func createKubeClient(kubeConfig string) (kubernetes.Interface, error) {
+	config, err := clientcmd.RESTConfigFromKubeConfig([]byte(kubeConfig))
+	if err != nil {
+		return nil, fmt.Errorf("unable to create RESTConfig using kubeconfig from RDE: [%v]", err)
+	}
+	return kubernetes.NewForConfig(config)
+}
