068bb5a24d9f25eb1ea311f0f59e4f31973af3c6
rliang88 <rliang@vmware.com>
2023-01-11 12:04:13 -0800

    [VKCI-91] dev: finished set implementation
    Signed-off-by: rliang88 <rliang@vmware.com>

--- pkg/testingsdk/set_utils.go
+++ pkg/testingsdk/set_utils.go
@@ -1,1 +1,26 @@
 package testingsdk
+
+var exists = struct{}{}
+
+type set struct {
+	m map[string]struct{}
+}
+
+func NewSet() *set {
+	s := &set{}
+	s.m = make(map[string]struct{})
+	return s
+}
+
+func (s *set) Add(value string) {
+	s.m[value] = exists
+}
+
+func (s *set) Contains(value string) bool {
+	_, c := s.m[value]
+	return c
+}
+
+func (s *set) Size() int {
+	return len(s.m)
+}
