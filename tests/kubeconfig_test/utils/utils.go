e28335db4a90d780da4701cafc9d3f7058d9b0e5
lzichong <lzichong@vmware.com>
2023-08-30 12:57:34 -0700

    Add small comment for util func
    Signed-off-by: lzichong <lzichong@vmware.com>

--- tests/kubeconfig_test/utils/utils.go
+++ tests/kubeconfig_test/utils/utils.go
@@ -2,6 +2,8 @@ package utils
 
 import "strings"
 
+// TrimTrailingSlash is used to trimming the final slash of directory/path's passed in.
+// Such that it can support paths like /tmp/ and /tmp as the inputs, and create a file output as /tmp/file.txt
 func TrimTrailingSlash(path string) string {
 	if strings.HasSuffix(path, "/") {
 		return path[:len(path)-1]
