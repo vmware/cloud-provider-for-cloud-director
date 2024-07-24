7544c77fed7289bc257d8a46986bbee5642ac60b
Aritra Sen <sena@vmware.com>
2024-07-24 16:15:49 -0700

    Use GetNsxtEdgeGatewayById instead of GetNsxtEdgeGatewayByName
    Signed-off-by: Aritra Sen <sena@vmware.com>

--- pkg/vcdsdk/gateway.go
+++ pkg/vcdsdk/gateway.go
@@ -1468,11 +1468,15 @@ func (gm *GatewayManager) IsNSXTBackedGateway() bool {
 // in case the code is unable to determine the required info, it will return false with an error.
 func (gm *GatewayManager) IsUsingIpSpaces() (bool, error) {
 	edgeGatewayName := gm.GatewayRef.Name
-	vdc := gm.Client.VDC
-	if vdc == nil {
-		return false, fmt.Errorf("unable to determine if gateway [%s] is using Ip Spaces or not. nil VDC object in client", edgeGatewayName)
+	edgeGatewayID := gm.GatewayRef.Id
+	clusterOrg, err := gm.Client.VCDClient.GetOrgByName(gm.Client.ClusterOrgName)
+	if err != nil {
+		return false, fmt.Errorf("error retrieving org [%s]: [%v]", gm.Client.ClusterOrgName, err)
 	}
-	edgeGateway, err := vdc.GetNsxtEdgeGatewayByName(edgeGatewayName)
+	if clusterOrg == nil || clusterOrg.Org == nil {
+		return false, fmt.Errorf("unable to determine if gateway [%s] is using Ip Spaces or not; obtained nil org", edgeGatewayName)
+	}
+	edgeGateway, err := clusterOrg.GetNsxtEdgeGatewayById(edgeGatewayID)
 	if err != nil {
 		return false, fmt.Errorf("unable to determine if gateway [%s] is using Ip Spaces or not. error [%v]", edgeGatewayName, err)
 	}
